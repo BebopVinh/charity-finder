93bed61449d5bf75119e92778ac4b8864da91e88
bebopvinh <vinhdo07@gmail.com>
2020-02-20 00:21:56 -0800

    add author credit for icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,6 +38,17 @@ const Layout = ({ children }) => {
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <br />
+          <p>
+            Icon made by
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+              Freepik
+            </a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </p>
         </footer>
       </div>
     </>
