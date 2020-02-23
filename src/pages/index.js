1d804d213f440c640a37a8a6f62831406c94db82
bebopvinh <vinhdo07@gmail.com>
2020-02-23 04:47:39 -0800

    add div wrapper to manage spacing with header

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,17 @@ import Layout from "../components/layout"
 import Splash from "../components/splash/splash"
 import SEO from "../components/seo"
 
-import Nav from "../components/nav"
-
 const IndexPage = () => (
   <Layout>
-    <Nav />
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <Link to="/page-2/">Go to page 2</Link>
-    <br />
-    <Link to="/page-3/">Go to page 3</Link>
-    <Splash />
-    {/* <button className="btn btn-blue">Blue Button</button> */}
+    <div className="my-4">
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <Link to="/page-2/">Go to page 2</Link>
+      <br />
+      <Link to="/page-3/">Go to page 3</Link>
+      <Splash />
+      {/* <button className="btn btn-blue">Blue Button</button> */}
+    </div>
   </Layout>
 )
 
