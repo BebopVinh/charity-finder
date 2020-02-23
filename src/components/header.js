86dadd1dec3408d9156dcfd2c4e2a1b68bcc57fb
bebopvinh <vinhdo07@gmail.com>
2020-02-23 04:44:45 -0800

    remove nav component and have header returns nav

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,41 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  // <header
+  //   style={{
+  //     background: `rebeccapurple`,
+  //     marginBottom: `1.45rem`,
+  //   }}
+  // >
+  //   <div
+  //     style={{
+  //       margin: `0 auto`,
+  //       maxWidth: 960,
+  //       padding: `1.45rem 1.0875rem`,
+  //     }}
+  //   >
+  //     <h1 style={{ margin: 0 }}>
+  //       <Link
+  //         to="/"
+  //         style={{
+  //           color: `white`,
+  //           textDecoration: `none`,
+  //         }}
+  //       >
+  //         {siteTitle}
+  //       </Link>
+  //     </h1>
+  //   </div>
+  // </header>
+  <nav id="header" className=".bg-green-300 mb-3">
+    <div className="nav-container container">
+      <div className="pl-4 flex items-center">
+        <a href="#" className="nav-link toggleColour">
+          {siteTitle ? siteTitle : "My Header"}
+        </a>
+      </div>
     </div>
-  </header>
+  </nav>
 )
 
 Header.propTypes = {
