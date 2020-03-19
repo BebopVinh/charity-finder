cd0fa577c34e0d2805050491ce6e8f77d19a0b7a
bebopvinh <vinhdo07@gmail.com>
2020-03-19 10:25:57 -0700

    add action in reducer to manage fetched orgs

--- src/state/app.js
+++ src/state/app.js
@@ -1,12 +1,18 @@
 const initialState = {
   isDarkMode: false,
+  organizations: [],
 }
 
 const TOGGLE_DARKMODE = "TOGGLE_DARKMODE"
+const GET_ORGANIZATIONS = "GET_ORGANIZATION"
 
 export const toggleDarkMode = isDarkMode => ({
   type: TOGGLE_DARKMODE,
   isDarkMode,
+})
+
+export const getOrganizations = () => ({
+  type: GET_ORGANIZATIONS,
 })
 
 export default (state = initialState, action) => {
@@ -16,6 +22,9 @@ export default (state = initialState, action) => {
       let newState = { ...state, isDarkMode: action.isDarkMode }
       console.log(newState)
       return newState
+    case GET_ORGANIZATIONS:
+      debugger
+      return
     default:
       return state
   }
