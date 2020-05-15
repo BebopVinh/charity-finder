81e4a0b7be08638064fe0cb53397cad307a02110
bebopvinh <vinhdo07@gmail.com>
2020-05-15 11:45:07 -0700

    add rendering of organizations as cards, fix fetch

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,47 +1,33 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import { connect } from "react-redux"
-
-import { getOrganizations } from "../state/app"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import OrganizationList from "../components/organization/organizationlist"
 
-const SecondPage = (props, { dispatch }) => {
+const SecondPage = props => {
   const { organizations } = props
   const [results, setResults] = useState("")
-  const apiKey = `?api_key=${process.env.GATSBY_API_KEY}`
-  const url = "/public/orgservice/all/organizations.json"
 
   async function fetchOrganizations() {
     const response = await fetch("http://localhost:3000/organizations")
     const data = await response.json()
     await setResults(data)
-    await console.log(results)
   }
 
   useEffect(() => {
-    // if (organizations.length === 0) {
-    // return fetch("/api" + url + apiKey, {
-    //   method: "GET",
-    //   mode: "cors",
-    //   headers: {
-    //     "Content-type": "application/json",
-    //   },
-    // })
-    //   .then(response => console.log(response))
-    //   .then(data => (results = data))
-    // }
-    debugger
-    fetchOrganizations()
-  })
+    console.log("results: ", results)
+  }, [results])
+
+  useEffect(() => {
+    if (results === "") fetchOrganizations()
+  }, [])
 
   return (
     <Layout>
       <SEO title="Page two" />
       <Link to="/">Go back to the homepage</Link>
-
-      <div></div>
+      {results === "" ? null : <OrganizationList {...results} />}
     </Layout>
   )
 }
