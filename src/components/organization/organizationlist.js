fb025fe2ccd51a5bc74548455b0291c6946e0f38
bebopvinh <vinhdo07@gmail.com>
2020-05-16 14:38:59 -0700

    use map index for key prop

--- src/components/organization/organizationlist.js
+++ src/components/organization/organizationlist.js
@@ -2,5 +2,7 @@ import React from "react"
 import OrganizationCard from "./organizationcard"
 
 export default function organizationList({ organizations }) {
-  return organizations.organization.map(org => <OrganizationCard org={org} />)
+  return organizations.organization.map((org, index) => (
+    <OrganizationCard org={org} key={index} />
+  ))
 }
