c422bdef1f7d50bfbef495cc742f15c38b920e4f
bebopvinh <vinhdo07@gmail.com>
2020-05-16 14:38:40 -0700

    remove use of "key" prop

--- src/components/organization/organizationcard.js
+++ src/components/organization/organizationcard.js
@@ -2,17 +2,19 @@ import React from "react"
 import Image from "../image"
 
 export default function OrganizationCard({ org }) {
-  console.log(org)
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg py-2" id={org.id}>
+    <div
+      className="max-w-sm rounded overflow-hidden shadow-lg py-2"
+      id={org.id}
+    >
       {org.logoUrl ? (
-        <img class="w-full" src={org.logoUrl} alt="Logo Image" />
+        <img className="w-full" src={org.logoUrl} alt="Logo Image" />
       ) : (
         <Image />
       )}
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{org.name}</div>
-        <p class="text-gray-700 text-base">{org.mission}</p>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{org.name}</div>
+        <p className="text-gray-700 text-base">{org.mission}</p>
       </div>
     </div>
   )
